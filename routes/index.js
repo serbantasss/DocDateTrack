57a9e61e7b014c274bb29364e04de81ce213d9e7
Serban Bantas <georgeserban.bantas@studbocconi.it>
2023-06-13 23:00:57 +0300

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,89 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
+const owner_controller = require("../controllers/ownerController");
+const document_controller = require("../controllers/documentController");
+const category_controller = require("../controllers/categoryController");
+
+//Render Home Page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/**
+ DOCUMENT ROUTES.
+ */
+
+//GET request to create owner.
+router.get("/document/create", document_controller.document_create_get);
+
+//POST request to create owner.
+router.get("/document/create", document_controller.document_create_post);
+
+//GET request to remove owner.
+router.get("/document/:id/delete", document_controller.document_delete_get);
+
+//POST request to remove owner.
+router.get("/document/:id/delete", document_controller.document_delete_post);
+
+//GET request to update owner.
+router.get("/document/:id/update", document_controller.document_update_get);
+
+//POST request to update owner.
+router.get("/document/:id/update", document_controller.document_update_post);
+
+//GET request for list of owners.
+router.get("/documents", document_controller.owner_list);
+
+/**
+  OWNER ROUTES.
+ */
+
+//GET request to create owner.
+router.get("/owner/create", owner_controller.owner_create_get);
+
+//POST request to create owner.
+router.get("/owner/create", owner_controller.owner_create_post);
+
+//GET request to remove owner.
+router.get("/owner/:id/delete", owner_controller.owner_delete_get);
+
+//POST request to remove owner.
+router.get("/owner/:id/delete", owner_controller.owner_delete_post);
+
+//GET request to update owner.
+router.get("/owner/:id/update", owner_controller.owner_update_get);
+
+//POST request to update owner.
+router.get("/owner/:id/update", owner_controller.owner_update_post);
+
+//GET request for list of owners.
+router.get("/owners", owner_controller.owner_list);
+
+/**
+ CATEGORY ROUTES.
+ */
+
+//GET request to create owner.
+router.get("/category/create", category_controller.category_create_get);
+
+//POST request to create owner.
+router.get("/category/create", category_controller.category_create_post);
+
+//GET request to remove owner.
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+//POST request to remove owner.
+router.get("/category/:id/delete", category_controller.category_delete_post);
+
+//GET request to update owner.
+router.get("/category/:id/update", category_controller.category_update_get);
+
+//POST request to update owner.
+router.get("/category/:id/update", category_controller.category_update_post);
+
+//GET request for list of owners.
+router.get("/categories", category_controller.category_list);
 
 module.exports = router;
