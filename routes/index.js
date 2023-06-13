3a15e98b3d3444ff9d19080c11f2c38179e1f380
Serban Bantas <georgeserban.bantas@studbocconi.it>
2023-06-14 00:33:06 +0300

    modified http methods process

--- routes/index.js
+++ routes/index.js
@@ -5,35 +5,58 @@ const router = express.Router();
 const owner_controller = require("../controllers/ownerController");
 const document_controller = require("../controllers/documentController");
 const category_controller = require("../controllers/categoryController");
+const asyncHandler = require("express-async-handler");
+const Document = require("../models/document");
+const Owner = require("../models/owner");
+const DocumentType = require("../models/category");
 
 //Render Home Page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/*exports.index = asyncHandler(async (req, res, next) => {
+  //res.send("NOT IMPLEMENTED: Site Home Page");
+  const [
+    numDocuments,
+    numOwners,
+    numDocTypes
+  ] = await Promise.all([
+    Document.countDocuments({}).exec(),
+    Owner.countDocuments({}).exec(),
+    DocumentType.countDocuments({}).exec(),
+  ])
+
+  res.render("index",{
+    title: "Document Tracking Home",
+    document_count: numDocuments,
+    owner_count: numOwners,
+    doctype_count: numDocTypes,
+  })
+});*/
 
 /**
  DOCUMENT ROUTES.
  */
 
-//GET request to create owner.
+//GET request to create document.
 router.get("/document/create", document_controller.document_create_get);
 
-//POST request to create owner.
+//POST request to create document.
 router.get("/document/create", document_controller.document_create_post);
 
-//GET request to remove owner.
+//GET request to remove document.
 router.get("/document/:id/delete", document_controller.document_delete_get);
 
-//POST request to remove owner.
+//POST request to remove document.
 router.get("/document/:id/delete", document_controller.document_delete_post);
 
-//GET request to update owner.
+//GET request to update document.
 router.get("/document/:id/update", document_controller.document_update_get);
 
-//POST request to update owner.
+//POST request to update document.
 router.get("/document/:id/update", document_controller.document_update_post);
 
-//GET request for list of owners.
+//GET request for list of documents.
 router.get("/documents", document_controller.owner_list);
 
 /**
@@ -65,25 +88,25 @@ router.get("/owners", owner_controller.owner_list);
  CATEGORY ROUTES.
  */
 
-//GET request to create owner.
+//GET request to create category.
 router.get("/category/create", category_controller.category_create_get);
 
-//POST request to create owner.
+//POST request to create category.
 router.get("/category/create", category_controller.category_create_post);
 
-//GET request to remove owner.
+//GET request to remove category.
 router.get("/category/:id/delete", category_controller.category_delete_get);
 
-//POST request to remove owner.
+//POST request to remove category.
 router.get("/category/:id/delete", category_controller.category_delete_post);
 
-//GET request to update owner.
+//GET request to update category.
 router.get("/category/:id/update", category_controller.category_update_get);
 
-//POST request to update owner.
+//POST request to update category.
 router.get("/category/:id/update", category_controller.category_update_post);
 
-//GET request for list of owners.
+//GET request for list of categories.
 router.get("/categories", category_controller.category_list);
 
 module.exports = router;
